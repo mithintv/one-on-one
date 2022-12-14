9042779adfca7e2f5f8847e8a2044d71061d050d
Mithin <mithingthomas@gmail.com>
2022-12-14 17:08:37 -0500

    C:/Program Files/Git/restrict command functional

--- src/listeners/commands.js
+++ src/listeners/commands.js
@@ -100,10 +100,100 @@ const frequency = async ({ client, command, ack, respond }) => {
   }
 };
 
+const restrict = async ({ client, command, ack, respond }) => {
+  try {
+    console.log(command);
+
+    // Acknowledge command request
+    await ack();
+
+    // Obtain user, channel_id, team_id and parameters
+    const { user_id, channel_id, team_id, text } = command;
+
+    // Check if bot is in the channel
+    const { bot_id, members, membership } = await checkBotMembership(command, client);
+
+    // If bot is not in channel, respond with failure, else use filtered members array to initiate function
+    if (!membership) {
+      await respond(`/restrict can only be called on channels that <@${bot_id}> has joined`);
+      return;
+    } else {
+
+      // Fetch installtion
+      let team = await fetchInstallation({}, team_id);
+      // If no parameters are set, output current restrictions
+      let restrict = team[channel_id][user_id].restrict;
+      if (!text && restrict.length === 0) {
+        await respond("You are currenlt being paired with everyone on this channel for one-on-one's with no restrictions.");
+      }
+      else if (!text) {
+        let response = "You are currently not being paired with the following members in this channel for one-on-one's:\n";
+        console.log(restrict);
+        restrict.forEach(element => {
+          response = response + `<@${element}>\n`;
+        });
+        await respond(response);
+      } else {
+        // Check if passed in members are members of the channel
+        const splitParams = text.replaceAll("@", "").split(" ");
+
+        // Get users list
+        const { members } = await client.users.list();
+
+        // Create object with keys corresponding to names and values corresponding to user_ids
+        const memberNames = {};
+        for (let i = 0; i < members.length; i++) {
+          if (!memberNames[members[i].name]) {
+            memberNames[members[i].name] = members[i].id;
+          }
+        }
+        console.log(memberNames);
+
+        // Loop through params and replace user names with user_ids
+        for (let i = 0; i < splitParams.length; i++) {
+          if (memberNames[splitParams[i]]) {
+            splitParams[i] = memberNames[splitParams[i]];
+          };
+        }
+        console.log(splitParams);
+
+        // Create a document that sets the restrict key of a specific user on a specific channel
+        const channel = team[channel_id];
+        const user = channel[user_id];
+        const updateDoc = {
+          $set: {
+            [channel_id]: {
+              ...channel,
+              [user_id]: {
+                ...user,
+                restrict: splitParams
+              },
+            }
+          },
+        };
+        const result = await updateInstallation(team_id, updateDoc);
+        console.log(result);
+        // Fetch updated installtion and updated restrictions
+        team = await fetchInstallation({}, team_id);
+        restrict = team[channel_id][user_id].restrict;
+        let response = "You are currently not being paired with the following members in this channel:\n";
+        restrict.forEach(element => {
+          response = response + `<@${element}>\n`;
+        });
+        await respond(response);
+      }
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 
 
 export default function registerCommands(app) {
 
   app.command('/pair', pair);
   app.command('/frequency', frequency);
+  app.command('/restrict', restrict);
 }
