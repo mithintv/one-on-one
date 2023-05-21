ad3a3383d7a58528f6a45a7496a61f8895d35181
Mithin <mithingthomas@gmail.com>
2023-05-21 18:19:57 -0400

    Wrap mongodb connection in async function

--- src/app.js
+++ src/app.js
@@ -4,8 +4,13 @@ import mongo from "./lib/mongo.js";
 // slack listeners
 import registerListeners from "./listeners/index.js";
 
-await mongo.connect();
-console.log('Successfully connected to database');
+
+// connect to db
+(async () => {
+  await mongo.connect();
+  console.log('Successfully connected to database');
+})();
+
 
 // registers commands for bot
 registerListeners(app);
