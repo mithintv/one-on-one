ece985518e5baa9864ad8ccfbcc945bdbd3e61a9
Mithin <mithingthomas@gmail.com>
2023-07-21 19:40:46 -0400

    Add testing parameters to constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,11 +1,17 @@
 let setTime, getTime, interval, first, span;
 
-if (process.env.ENVIRO === 'testing') {
+if (process.env.ENVIRO === 'local') {
   setTime = 'setMinutes';
   getTime = 'getMinutes';
   interval = process.env.INTERVAL;
   first = 2;
   span = "minutes";
+} else if (process.env.ENVIRO === 'testing') {
+  setTime = 'setDate';
+  getTime = 'getDate';
+  interval = 1;
+  first = 1;
+  span = "days";
 } else {
   setTime = 'setDate';
   getTime = 'getDate';
